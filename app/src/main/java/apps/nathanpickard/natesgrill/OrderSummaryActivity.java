45bd7052c753a9be7dcded1f2f5a18d27af6ea4d
Nathan Pickard <nathan.pickard87@gmail.com>
2016-06-24 22:11:22 -0700

    Began adding intent for email receipt

--- app/src/main/java/apps/nathanpickard/natesgrill/OrderSummaryActivity.java
+++ app/src/main/java/apps/nathanpickard/natesgrill/OrderSummaryActivity.java
@@ -1,6 +1,7 @@
 package apps.nathanpickard.natesgrill;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -31,6 +32,16 @@ public class OrderSummaryActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(OrderSummaryActivity.this, "Order sent!", Toast.LENGTH_LONG).show();
+
+                // Add intent for email
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:")); // Only email apps should handle this
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Nate's Wings order for " + name);
+                intent.putExtra(Intent.EXTRA_TEXT, message);
+
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
             }
         });
 
